8265bbd57da0a1662540492b4f27db346b21ede2
Maksym Nevdashchenko <nevmaks@gmail.com>
2023-04-13 09:35:21 +0300

    Configured store with middleware

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,4 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from "../Reducer";
+import thunk from "redux-thunk";
 
-export const store = configureStore({reducer: rootReducer});
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)
+});
